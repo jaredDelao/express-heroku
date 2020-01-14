40a7bfac957a082dbbc5790b990ce84187a97146
JARED DE LA O HERNANDEZ <jdeo@ad.elektra.com.mx>
2020-01-14 12:35:57 -0600

    despliegue

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
-app.use( express.static( __dirname + '/public'));
+const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-    // res.send('Hello')
-    let salida = {
-        nombre: "Jared",
-        edad: 22,
-        url: req.url
-      };
-    res.send(salida);
+app.use(express.static(__dirname + '/public'));
+require('./hbs/helpers/helpers');
+
+// HBS
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+
+
+app.get('/', function (req, res) {
+    res.render('home', {
+        nombre: 'Jared',
+        anio: new Date().getFullYear()
+    });
 });
 
-app.listen(3000, ()=> {
-    console.log('Escuchando peticiones en el puerto 3000');
+app.get('/about', function (req, res) {
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 });
